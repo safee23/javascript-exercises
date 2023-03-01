2a8ca28e923ee1c33950fd947af61a33bf4d56fb
Safeeullah <safee106@gmail.com>
2023-03-01 15:10:33 -0600

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (typeof int1 !== 'number' || typeof int2 !== 'number'){
+        return 'ERROR';
+    }
+    else if (int1 < 0 || int2 < 0){
+        return 'ERROR';
+    }
+    let output = 0;
+    for (let i = Math.min(int1, int2); i <= Math.max(int1, int2); i++){
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
