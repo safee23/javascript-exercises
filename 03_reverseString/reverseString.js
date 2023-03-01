58188edb42cd2f96aa1477ee86505ada5bfb786c
Safeeullah <safee106@gmail.com>
2023-03-01 10:19:57 -0600

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+   let output =  '';
+//    let i = 0;
+   while (word) {
+    letter = word.slice(word.length - 1);
+    output += letter;
+    word = word.slice(0, -1)
+   }
+   return output;
 };
 
 // Do not edit below this line
